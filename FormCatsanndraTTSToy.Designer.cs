2ba0996eb5bab94b88406a6b763e694e32173865
Ann <ann@poppingspree.dev>
2022-04-07 20:24:43 -0500

    Initial commit

---
+++ FormCatsanndraTTSToy.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Catsanndra_TTS_Toy
+{
+    partial class frmCatsanndraTTSToy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCatsanndraTTSToy));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.txtDebugOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(13, 13);
+            this.txtInput.MaxLength = 1000000000;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(433, 425);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = resources.GetString("txtInput.Text");
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(453, 13);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(453, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(453, 71);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(453, 99);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // txtDebugOutput
+            // 
+            this.txtDebugOutput.Location = new System.Drawing.Point(453, 127);
+            this.txtDebugOutput.MaxLength = 1000000000;
+            this.txtDebugOutput.Multiline = true;
+            this.txtDebugOutput.Name = "txtDebugOutput";
+            this.txtDebugOutput.Size = new System.Drawing.Size(335, 311);
+            this.txtDebugOutput.TabIndex = 5;
+            // 
+            // frmCatsanndraTTSToy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDebugOutput);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmCatsanndraTTSToy";
+            this.Text = "Catsanndra TTS Toy";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmCatsanndraTTSToy_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox txtDebugOutput;
+    }
+}
+
---
+++ FormCatsanndraTTSToy.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using SpeechLib;
+using System.Net;
+
+namespace Catsanndra_TTS_Toy
+{
+    public partial class frmCatsanndraTTSToy : Form
+    {
+        SpVoice voice = new SpVoice();
+        ThreadStart thRefSpeak;
+        Thread thSpeak;
+
+        ThreadStart thRefListen;
+        Thread thListen;
+        ISpeechObjectTokens voices;
+
+        string portNum = "8734";
+
+        bool quitNow = false;
+
+        HttpListener httpListener = new HttpListener();
+
+        public frmCatsanndraTTSToy()
+        {
+            InitializeComponent();
+            this.voice = new SpVoice();
+            this.thRefSpeak = new ThreadStart(SpeakCurrentText);
+            this.thSpeak = new Thread(thRefSpeak);
+
+            this.thRefListen = new ThreadStart(ListenForHttpRequests);
+            this.thListen = new Thread(thRefListen);
+
+            voices = voice.GetVoices();
+            foreach (SpObjectToken voiceOption in voices)
+            {
+                WriteLine(voiceOption.Id);
+            }
+
+            string thePrefix = "http://localhost:" + portNum + "/";
+            httpListener.Prefixes.Add(thePrefix);
+
+            this.thListen.Start();
+
+            WriteLine("Listening for messages on " + thePrefix);
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            
+
+            this.thSpeak = new Thread(this.thRefSpeak);
+            this.thSpeak.Start();
+        }
+
+        public void SpeakCurrentText() 
+        {
+            voice.Skip("Sentence", 9999);
+            voice.Speak(txtInput.Text, SpeechVoiceSpeakFlags.SVSFDefault);
+        }
+
+        public void SpeakText(string textToBeSpoken)
+        {
+            voice.Skip("Sentence", 9999);
+            voice.Speak(textToBeSpoken, SpeechVoiceSpeakFlags.SVSFDefault);
+        }
+
+        public void WriteLine(string txt) 
+        {
+            txtDebugOutput.Text = txt + "\r\n" + txtDebugOutput.Text;
+        }
+
+        public void HandleHttpRequest(IAsyncResult result) 
+        {
+            //WriteLine("Received HTTP Request.");
+
+            HttpListener listener = (HttpListener)result.AsyncState;
+            // Call EndGetContext to complete the asynchronous operation.
+            HttpListenerContext context = listener.EndGetContext(result);
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+
+            string sayThis = request.QueryString.Get("speak");
+
+            // Construct a response.
+            string responseString = "<HTML><BODY>";
+            responseString += "Hello world!<br/>";
+            responseString += "You want me to say: " + sayThis;
+            responseString += "</BODY></HTML>";
+        
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+
+            output.Close();
+
+            if (sayThis != null && sayThis != "") 
+            {
+                SpeakText(sayThis);
+            }
+        }
+
+        public void ListenForHttpRequests()
+        {
+            httpListener.Start();
+            while (!quitNow) 
+            {
+                IAsyncResult result = httpListener.BeginGetContext(new AsyncCallback(HandleHttpRequest), httpListener);
+                result.AsyncWaitHandle.WaitOne();
+            }
+        }
+
+        private void frmCatsanndraTTSToy_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SpeakText("");
+            this.quitNow = true;
+            thSpeak.Abort();
+            thListen.Abort();
+        }
+    }
+}
